c5ee709764b8fd7591b03db0313199f9b936dc61
grd349 <grd349@gmail.com>
2018-07-13 15:45:40 +0100

    Added RF experiment and started to setup package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='chaplinfilter',
+      version=0.1,
+      author='Guy R. Davies',
+      author_email='grd349@gmail.com',
+      licence='MIT',
+      packages=['chaplinfilter'],
+      zip_safe=False)
