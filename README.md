03600abb0419bbe4331fcccb2c398e7c7f5113b5
grd349 <grd349@gmail.com>
2018-07-20 11:41:16 +0100

    Uploaded 0.1 to pypi via twine - see upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ChaplinFilter
 An ML implementation of the function that maps fundamental properties to suggested time domain filtering timescales.
+
+See Chaplin et al. in press for full details TODO.
--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
 setup(name='chaplinfilter',
-      version=0.1,
+      version='0.1',
       author='Guy R. Davies',
       author_email='grd349@gmail.com',
       licence='MIT',
       packages=['chaplinfilter'],
-      zip_safe=False)
+      #url='TODO',
+      dependencies=['numpy', 'sklearn', 'pandas'])
