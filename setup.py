005fb4143aabee16eb47619f9184ef19c5eacaac
grd349 <grd349@gmail.com>
2018-07-20 14:01:07 +0100

    Fixed path to data file and removed NN work

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='chaplinfilter',
-      version='0.1.2',
+      version='0.1.3',
       author='Guy R. Davies',
       author_email='grd349@gmail.com',
       licence='MIT',
